a4a53ff26e01908aff04cb6b4bb1b9e081a32688
Gilnanderson Barrozo <79672107+Gilnanderson@users.noreply.github.com>
2021-02-27 13:03:14 -0300

    Update dice_roller.py
    Improved the code in case of a draw.

--- dice_roller.py
+++ dice_roller.py
@@ -1,18 +1,41 @@
-import random
 def main():
-  dice_rolls = int(input('How many dice would you like to roll?'))
-  dice_size = int(input('How many sides are the dice?'))
-  dice_sum = 0
-  for i in range(0, dice_rolls):
-    roll = random.randint(1, dice_size)
-    dice_sum = dice_sum + roll
-    if roll == 1:
-      print(f'You rolled a {roll}! Critical Fail!')
-    elif roll == dice_size:
-      print(f'You rolled a {roll}! Critical Success!')
+    players = int(input("How many players are rolling the dice?"))
+    dice_rolls = int(input('How many dice would you like to roll?'))
+    dice_size = int(input('How many sides are the dice?'))
+    dice_sum = 0
+    
+    total = 0
+    vencedor = [0]
+    for x in range(0, players):
+        player = x + 1
+        for i in range(0, dice_rolls):
+            roll = random.randint(1, dice_size)
+            dice_sum = dice_sum + roll
+            if roll == 1:
+        	    print(f'Player {player} rolled a {roll}! Critical Fail!')
+            elif roll == dice_size:
+                print(f'Player {player} rolled a {roll}! Critical Success!')
+            else:
+                print(f'Player {player} rolled a {roll}')
+        print(f'Player {player} has rolled a total of {dice_sum}')
+        if total < dice_sum:
+            total = dice_sum
+            
+            vencedor[0] = player
+            dice_sum = 0
+        elif total == dice_sum:
+            vencedor.append(player)
+        dice_sum = 0
+    if len(vencedor) == 1:
+        print(f'Player {vencedor[0]} is the winner')
     else:
-      print(f'You rolled a {roll}')
-  print(f'You have rolled a total of {dice_sum}')
+         print("The winners are", end=" ")
+         for i in range(len(vencedor)):
+            if vencedor[i] != vencedor[-1]:
+                print(f"Player {vencedor[i]},", end=" ")
+            else:
+                print("Player", vencedor[i])
+
 
 if __name__== "__main__":
-  main()
+    main()
